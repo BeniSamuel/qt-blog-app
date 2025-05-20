e2d957f6c142df7d89b577291fc543ab9fcc637b
benisamuel <benisamuel566@gmail.com>
2025-05-20 20:42:38 +0200

    initial commit

---
+++ backend/src/main/java/com/benisamuel/qtblog/QtblogApplication.java
@@ -0,0 +1,13 @@
+package com.benisamuel.qtblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QtblogApplication.class, args);
+    }
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p className=' text'>Hello world</p>
+    </div>
+  )
+}
+
+export default App
