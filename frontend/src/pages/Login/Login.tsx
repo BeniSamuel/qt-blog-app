307a625b275b66d61011ee35a043b4340e7b8ec9
benisamuel <benisamuel566@gmail.com>
2025-05-20 21:09:33 +0200

    added login and signup pages

---
+++ frontend/src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../../layouts/form/Form";
+
+const Login: React.FC = () => {
+  return (
+    <Form>
+      <div>Hello world</div>
+    </Form>
+  );
+};
+
+export default Login;
