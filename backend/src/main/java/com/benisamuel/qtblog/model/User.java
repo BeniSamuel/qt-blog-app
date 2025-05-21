2dd3de940fc3fedbc04e6db16a1364fa559ba0cc
benisamuel <benisamuel566@gmail.com>
2025-05-21 14:30:42 +0200

    added user model

---
+++ backend/src/main/java/com/benisamuel/qtblog/model/User.java
@@ -0,0 +1,29 @@
+package com.benisamuel.qtblog.model;
+
+import com.benisamuel.qtblog.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
