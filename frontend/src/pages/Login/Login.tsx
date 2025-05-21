11f9e0633ee68bef1d7dce20f53b39155c94fcb6
benisamuel <benisamuel566@gmail.com>
2025-05-21 10:22:37 +0200

    improved login outlook

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -5,14 +5,14 @@ import Checkbox from "../../components/form/checkbox/Checkbox";
 const Login: React.FC = () => {
   return (
     <Form>
-      <div>
+      <div className=" flex flex-col justify-center gap-4">
         <div className=" leading-7">
           <div className=" font-urbanist font-bold text-2xl">Welcome back!</div>
           <div className=" text-[#696778] font-urbanist">
             Enter your credentials to proceed
           </div>
         </div>
-        <form>
+        <form className=" flex flex-col gap-3">
           <div className=" flex flex-col gap-2">
             <label className=" text-[#696778] font-urbanist">
               Email address
@@ -31,11 +31,22 @@ const Login: React.FC = () => {
               className=" placeholder:font-urbanist placeholder:text-[#DDDDDD] border-2 border-[#DDDDDD] py-3 px-4 rounded-md"
             />
           </div>
-          <div>
+          <div className=" flex flex-row justify-between">
             <div className=" flex flex-row gap-2 items-center">
               <Checkbox />
               <span className=" font-urbanist text-[#696778]">Remember me</span>
             </div>
+            <div className=" text-[#696778] font-urbanist underline">
+              Forgot password?
+            </div>
+          </div>
+
+          <button className=" bg-[#143751] text-white font-urbanist py-3 rounded-md my-8">
+            Sign in
+          </button>
+          <div className=" font-urbanist text-[#696778] flex flex-row justify-center gap-1">
+            Don't have an account?{" "}
+            <span className=" text-[#696778] font-semibold">Create one</span>
           </div>
         </form>
       </div>
