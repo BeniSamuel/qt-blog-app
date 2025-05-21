c8c51d2f4a748c74f0076ca339b473e38cc6ef12
benisamuel <benisamuel566@gmail.com>
2025-05-21 14:07:23 +0200

    added home component

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/dashboard/navbar/Navbar'
+import Intro from '../../components/dashboard/Intro/Intro'
+
+const Home:React.FC = () => {
+  return (
+    <div>
+        <Navbar />
+        <Intro />
+    </div>
+  )
+}
+
+export default Home
