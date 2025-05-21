75e82891594a27e52c3b23e6ba2c777bbf61eca3
benisamuel <benisamuel566@gmail.com>
2025-05-21 10:21:58 +0200

    improved form components

--- frontend/src/layouts/form/FormLeft.tsx
+++ frontend/src/layouts/form/FormLeft.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import bg_cover from "../../assets/form/formLeftBg.png";
+import qtblog from "../../assets/form/qtblog.svg";
 
 const FormLeft: React.FC = () => {
   return (
-    <div className="w-1/2 h-full">
+    <div className="hidden md:block md:w-1/2 h-full relative">
       <img
         src={bg_cover}
         alt="Form Background"
         className="w-full h-full object-cover"
       />
+      <div>
+        <div className="absolute bottom-28 left-12">
+          <img src={qtblog} />
+        </div>
+        <div className="absolute bottom-7 left-12">
+          <p className="font-urbanist text-white font-semibold text-xl">
+            Interesting ideas worth reading, <br />
+            from Rwanda's top authors
+          </p>
+          <p className="text-[#DDDDDD] font-urbanist font-medium">
+            Join the world of 100+ creative writers and more
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/layouts/form/FormRight.tsx
+++ frontend/src/layouts/form/FormRight.tsx
@@ -6,7 +6,7 @@ type FormRightProps = {
 
 const FormRight: React.FC<FormRightProps> = ({ children }) => {
   return (
-    <div className="w-1/2 h-full overflow-auto p-10 flex items-center justify-center bg-white">
+    <div className="w-full md:w-1/2 h-full overflow-auto p-10 flex items-center justify-center bg-white">
       <div className="w-full max-w-md">{children}</div>
     </div>
   );
