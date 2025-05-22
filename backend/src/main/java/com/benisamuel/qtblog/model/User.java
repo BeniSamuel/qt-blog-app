82b3258058a879b922e058182a0318241c69682b
benisamuel <benisamuel566@gmail.com>
2025-05-22 11:18:06 +0200

    Added a custom user constructor for proper initialization

--- backend/src/main/java/com/benisamuel/qtblog/model/User.java
+++ backend/src/main/java/com/benisamuel/qtblog/model/User.java
@@ -18,12 +18,22 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     @Column(unique = true)
     private String email;
+
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    public User (String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
 }
