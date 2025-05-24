24b9612c29738a3cf1c66a60a07aa5ddaa0b9bd5
benisamuel <benisamuel566@gmail.com>
2025-05-24 22:09:59 +0200

    improved navbar responsiveness

--- frontend/src/components/dashboard/navbar/Navbar.tsx
+++ frontend/src/components/dashboard/navbar/Navbar.tsx
@@ -5,15 +5,15 @@ import UserProfile from "./UserProfile";
 
 const Navbar: React.FC = () => {
   return (
-    <div className=" flex flex-row justify-between items-center py-5 px-12">
-      <div className=" flex flex-row gap-8">
-        <img src={blogLogo} />
-        <div className=" font-urbanist font-semibold">Home</div>
-        <div className=" font-urbanist font-medium">Blogs</div>
-        <div className=" font-urbanist font-medium">Add blog</div>
+    <div className=" sticky top-0 left-0 right-0 z-50 bg-white/80  px-6 md:px-12 py-4 flex justify-between items-center">
+      <div className="flex flex-row gap-6 md:gap-10 items-center">
+        <img src={blogLogo} alt="Blog Logo" className="h-8 w-auto" />
+        <div className="text-sm md:text-base font-urbanist font-semibold cursor-pointer">Home</div>
+        <div className="text-sm md:text-base font-urbanist font-medium cursor-pointer">Blogs</div>
+        <div className="text-sm md:text-base font-urbanist font-medium cursor-pointer">Add blog</div>
       </div>
-      <div className=" flex flex-row items-center gap-4">
-        <img src={notification}/>
+      <div className="flex flex-row items-center gap-4">
+        <img src={notification} alt="Notifications" className="h-6 w-6 cursor-pointer" />
         <UserProfile />
       </div>
     </div>
