7d47e37be9bbd58281b0260aa601ae0c739aebe5
benisamuel <benisamuel566@gmail.com>
2025-05-24 22:12:30 +0200

    Removed the content issue on post

--- backend/src/main/java/com/benisamuel/qtblog/model/Post.java
+++ backend/src/main/java/com/benisamuel/qtblog/model/Post.java
@@ -32,7 +32,7 @@ public class Post {
 
     public Post (String title, String content, String cover, LocalDateTime posted_at, User user) {
         this.title = title;
-        this.cover = content;
+        this.content = content;
         this.cover = cover;
         this.posted_at = posted_at;
         this. user = user;
