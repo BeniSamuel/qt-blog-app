1f092ee7aa798473b62389d262ea5204081a84af
benisamuel <benisamuel566@gmail.com>
2025-05-24 22:10:31 +0200

    added blog component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/dashboard/navbar/Navbar";
 import Intro from "../../components/dashboard/Intro/Intro";
+import Blogs from "../../components/dashboard/blogs/Blogs";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
       <Intro />
+      <Blogs />
     </div>
   );
 };
