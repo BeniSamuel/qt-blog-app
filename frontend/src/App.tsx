598fe3ca0d55d17ca5b2f4509e8d0c4c92ea7683
benisamuel <benisamuel566@gmail.com>
2025-05-25 18:19:41 +0200

    added blog pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
+import Blogs from "./pages/Blogs/Blogs";
+import Blog from "./pages/Blogs/Blog";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +13,8 @@ const App: React.FC = () => {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blog/:id" element={<Blog />} />
       </Routes>
     </Router>
   );
